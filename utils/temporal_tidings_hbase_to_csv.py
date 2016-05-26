e7371d140811069f05b2f02847b38a2eef6ca0b7
Anurag El Dorado <annurag94@gmail.com>
2016-05-26 18:43:40 +0530

    added utils

---
+++ utils/temporal_tidings_hbase_to_csv.py
@@ -0,0 +1,44 @@
+def write_csv(metric, start_year, end_year_plus_one, interval, metric_dict):
+	all_comm = ['AI', 'ALGO', 'ARC', 'BIO', 'CV', 'DB', 'DIST', 'DM', 'GRP', 'HCI', 'IR', 'ML', 'MUL', 'NLP', 'NW', 'OS', 'PL', 'RT', 'SC', 'SE', 'SEC', 'SIM', 'WWW']
+	csv = open(metric + '.csv', 'w')
+	csv.write('start_year,end_year+1,' + ','.join(all_comm) + '\n')
+	for year in range(start_year, end_year_plus_one, interval):
+		csv.write(str(year) + ',' + str(year + interval) + ',')
+		for comm in all_comm:
+			if (str(year), comm, metric) in metric_dict.keys():
+				csv.write(metric_dict[(str(year), comm, metric)] + ',')
+			else:
+				csv.write('0,')
+		csv.write('\n')
+
+org_file = open('TemporalTidings.txt')
+lines = org_file.readlines()
+
+print lines[0]
+lines = [line.strip() for line in lines]
+print lines[0]
+lines = [line.split(',')[0] + line.split(',')[2] for line in lines]
+print lines[0]
+lines = [line.split(' ')[0][0:4] + ' ' + line.split(' ')[0][10:] + line[line.find(' '):] for line in lines]
+print lines[0]
+
+metric_dict = {}
+
+for line in lines:
+	split_line = line.split(' ')
+	year = split_line[0]
+	comm = split_line[1]
+	metric = split_line[2].split(':')[1]
+	value = split_line[3].split('=')[1]
+	metric_dict[(year, comm, metric)] = value
+
+
+
+metrics = ['inwardness', 'cut', 'expansion', 'count']
+start_year = 1960
+end_year_plus_one = 2016
+interval = 1
+
+for metric in metrics:
+	write_csv(metric, start_year, end_year_plus_one, interval, metric_dict)
+	pass
